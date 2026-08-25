fdc53f361bdc9774a15240d02ae0afa2d6643ecf
Nunthaporn <165544295+Nunthaporn@users.noreply.github.com>
2026-08-25 16:38:20 +0700

    Load Easy Lean title position override last

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import "../../apps/vvic/frontend/src/interactions.css";
 import "../../apps/easy-lean/frontend/src/styles.css";
 import "./shell.css";
 import "./easy-lean-overrides.css";
+import "./easy-lean-title-position.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
