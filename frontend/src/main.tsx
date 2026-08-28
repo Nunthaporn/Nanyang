109931ed5372549de1747429bfa64205139b5527
Nunthaporn <165544295+Nunthaporn@users.noreply.github.com>
2026-08-28 11:22:33 +0700

    Use local dashboard styles after removing submodules

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "../../apps/vvic/frontend/src/index.css";
-import "../../apps/vvic/frontend/src/interactions.css";
-import "../../apps/easy-lean/frontend/src/styles.css";
 import "./shell.css";
 import "./vvic-overrides.css";
 import "./easy-lean-overrides.css";
